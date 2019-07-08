aa9f7df6b722e61a90fb4dd9761ce21f901c63e4
Derek Hutson <denverd1301@gmail.com>
2019-07-08 16:47:03 -0600

    Added deck shuffle function... may need to edit in future if array of objects is not functional

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const sortedDeck = []
+const shuffledDeck = []
 const suits = ["spades", "diamonds", "clubs", "hearts"];
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
@@ -8,9 +9,18 @@ const buildDeck = () => {
 	{
 		for(let x = 0; x < values.length; x++)
 		{
-			const card = {Value: values[x], Suit: suits[i]};
+			const card = {value: values[x], suit: suits[i]};
 			sortedDeck.push(card);
 		}
 	}
-	return deck;
+	return sortedDeck;
+}
+
+const shuffleDeck = () => {
+    while(sortedDeck.length > 0){
+        let cardIndex = Math.floor(Math.random()*sortedDeck.length);
+        let shuffledCard = sortedDeck.splice(cardIndex, 1);
+        shuffledDeck.push(shuffledCard);
+    }
+    return shuffledDeck;
 }
