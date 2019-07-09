fb770dc87008a60f50ac231ea74773263b2f2fdc
Derek Hutson <denverd1301@gmail.com>
2019-07-09 12:02:07 -0600

    Added deck shuffle function

--- js/app.js
+++ js/app.js
@@ -3,24 +3,34 @@ const shuffledDeck = []
 const suits = ["spades", "diamonds", "clubs", "hearts"];
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-const buildDeck = () => {
-
-	for(let i = 0; i < suits.length; i++)
+class Deck {
+	constructor() {
+	  this.deck = [];
+  
+	  const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+	  const values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+  
+	  for(let i = 0; i < suits.length; i++)
 	{
 		for(let x = 0; x < values.length; x++)
 		{
 			const card = {value: values[x], suit: suits[i]};
-			sortedDeck.push(card);
+			this.deck.push(card);
 		}
 	}
-	return sortedDeck;
-}
+	}
+  }
 
-const shuffleDeck = () => {
-    while(sortedDeck.length > 0){
-        let cardIndex = Math.floor(Math.random()*sortedDeck.length);
-        let shuffledCard = sortedDeck.splice(cardIndex, 1);
-        shuffledDeck.push(shuffledCard);
-    }
-    return shuffledDeck;
+const deck1 = new Deck
+console.log(deck1.deck)
+
+// thank you fisher yates shuffle method!
+const shuffle = (array) => {
+	const shuffledDeck = []
+	let cards = array.length, i;
+	while(cards > 0){
+		i = Math.floor(Math.random()*cards--)
+		shuffledDeck.push(array.splice(i, 1)[0]);
+	}
+	return shuffledDeck;
 }
