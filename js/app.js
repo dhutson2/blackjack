085d3e5d1192cf619270dac5d2b3ed950e5c3298
Derek Hutson <denverd1301@gmail.com>
2019-07-11 13:45:14 -0600

    Added a couple event listeners, updated home page and some styling

--- js/app.js
+++ js/app.js
@@ -2,26 +2,33 @@ let shuffledDeck = []
 const usedCards = []
 const suits = ["spades", "diamonds", "clubs", "hearts"];
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
 const modal = $("#myModal")[0];
-
-// Get the button that opens the modal
 const btn = $("#instruction-button")[0];
-
-// Get the <span> element that closes the modal
 const span = $(".close")[0];
+const $shuffle = $('#shuffle')
 
-// When the user clicks on the button, open the modal 
+$(document).ready(function() {
+	console.log('fully loaded!')
+	$('#home-page').show()
+	$('#cards').hide()
+   });
+
+$('#shuffle').click(function(){
+	$('#home-page').hide()
+	$('#title').hide()
+	shuffleAndDeal();
+	$('#instruction-button').hide()
+	$('#cards').show()
+  })
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
