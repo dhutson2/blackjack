2326f6250b0b4411509b220a74c53db6957d4901
dhutson2 <hutsond1301@gmail.com>
2019-10-21 12:27:11 -0600

    added early stage deal cards function

--- Py_Scripts/test.py
+++ Py_Scripts/test.py
@@ -7,6 +7,8 @@ deck = []
 suits = ["D", "S", "C", "H"]
 values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 
+# fix this ace later
+
 
 def make_deck():
     for val in values:
@@ -16,4 +18,20 @@ def make_deck():
             card = suit, val
             deck.append(card)
     random.shuffle(deck)
+
+
+dealer_hand = []
+player_hand = []
+
+
+def deal_cards():
+    dealer_hand.append(deck[:2])
+    player_hand.append(deck[2:4])
+    del deck[:4]
+    print(dealer_hand, player_hand)
     print(deck)
+
+
+def deal_cards_test():
+    make_deck()
+    deal_cards()
